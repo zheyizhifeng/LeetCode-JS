77593f02e5b97c6e585d4ad403e7e46002389325
lucida <zheyizhifeng@126.com>
2025-07-21 10:32:34 +0800

    整理3 数和 4 数之和问题

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -11,28 +11,30 @@ var threeSum = function (nums) {
   if (nums.length < 3) return [];
   nums.sort((a, b) => a - b);
   let result = [];
-  for (let i = 0; i < nums.length - 2; i++) { // 加数1
-    if(nums[i] > 0) break;
+  for (let i = 0; i < nums.length - 2; i++) { // 加数1,即nums[i]
+    if(nums[i] > 0) break; // 总和为 0, 首元素已 > 0, 跳过
     if (nums[i] === nums[i - 1] && i >= 1) { // 剪枝，跳过相同元素
       continue;
     }
-    let start = i + 1; // 加数2
-    let end = nums.length - 1; // 加数3
-    const diff = -nums[i];
+    let start = i + 1; // 加数2，即nums[j]
+    let end = nums.length - 1; // 加数3,即nums[k]
+    const diff = -nums[i]; // 剩余的和
     
+    // 因为已经排过序，所以可以双指针遍历
     while (start < end) {
       if (nums[start] + nums[end] < diff) {
         start++;
       } else if (nums[start] + nums[end] > diff) {
         end--;
-      } else { // 命中，但要过滤重复组合。
+      } else { 
+        // 命中一组结果，但要过滤掉重复组合。
+        // 分别用 low 和 high 记录当前重复的值
+        let low = nums[start], high = nums[end];
         result.push([nums[i], nums[start], nums[end]]);
-        start++;
-        end--;
-        while (start < end && nums[start] === nums[start - 1]) {
+        while (start < end && nums[start] === low) {
           start++;
         }
-        while (start < end && nums[end] === nums[end + 1]) {
+        while (start < end && nums[end] === high) {
           end--;
         }
       }
--- 18.四数之和.js
+++ 18.四数之和.js
@@ -11,13 +11,22 @@
  * @return {number[][]}
  */
 var fourSum = function (nums, target) {
-  nums = nums.sort((a, b) => a - b);
+  // 升序排序
+  nums.sort((a, b) => a - b);
+  // 少于 4 个
   if (nums.length < 4) return [];
+  // 结果数组
   let result = [];
+  // 第一个加数,nums[i]
   for (let i = 0; i < nums.length - 3; i++) {
-    if (i > 0 && nums[i] == nums[i - 1]) continue;
+    // -------------由于存在负数，负数相加，和可能更小，所以不能直接判断 nums[i] > target-----------
+    //  nums[i] 跳过重复
+    if (i >= 1 && nums[i] == nums[i - 1]) continue;
+    // 第二个加数,nums[j]
     for (let j = i + 1; j < nums.length - 2; j++) {
+      // nums[j] 跳过重复
       if (j - i > 1 && nums[j] == nums[j - 1]) continue;
+      // 第三第四个加数, nums[j] 和 nums[k]
       let start = j + 1;
       let end = nums.length - 1;
       while (start < end) {
@@ -27,11 +36,16 @@ var fourSum = function (nums, target) {
         } else if (nums[start] + nums[end] < diff) {
           start++;
         } else {
-          if (end === nums.length - 1 || nums[start] !== nums[start - 1] || nums[end] !== nums[end + 1]) {
-            result.push([nums[i], nums[j], nums[start], nums[end]]);
+          // 找到一组答案
+          result.push([nums[i], nums[j], nums[start], nums[end]]);
+          // 去重
+          const low = nums[start], high = nums[end];
+          while (start < end && nums[start] === low) {
+            start++;
           }
-          start++;
-          end--;
+          while (start < end && nums[end] === high) {
+            end--;
+          }
         }
       }
     }
